e05e61ce58fb691d3f385574beeff2a0f57422e1
Ramsey Louder <RamseyLouder@gmail.com>
2024-05-14 16:08:33 -0400

    added used car class

---
+++ GitClassProject/UsedCar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitClassProject
+{
+    public class UsedCar: Car
+    {
+        //constructor
+        public double Mileage { get; set; }
+        public UsedCar(string make, string model, int year, decimal price, double mileage):base( make, model,  year, price)
+        {
+            Mileage = mileage;
+        }
+        //method
+        public override string ToString()
+        {
+            return $"{Make}\t{Model}\t{Year}\t{Price} {Mileage}";
+        }
+
+
+    }
+}
